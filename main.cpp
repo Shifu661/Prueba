bc95416b5503c26323a4cb22dc7ec9eb1929fbcc
Yoshiro Miranda <Yoshiro Miranda@LAPTOP-SBPN7OVU>
2020-06-16 21:07:15 -0500

    lala

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main
+{
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+    cout<<"ja";
+
+
+    return 0;
+}
